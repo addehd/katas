f2f80d05944129947bc0630f5f044b2411d1215f
addehd <hej@adolfo.se>
2022-03-06 00:19:00 +0100

    calculate number of days lived

---
+++ age-in-days.js
@@ -0,0 +1,16 @@
+// Did you ever want to know how many days old are you? 
+//Complete the function which returns your age in days. 
+//The birthday is given in the following order: year, month, day.
+
+//For example if today is 30 November 2015 then
+//ageInDays(2015, 11, 1) returns "You are 29 days old"
+
+
+function ageInDays(year, month, day) {
+  const today = new Date();
+  const birthDate = new Date(year, month - 1, day);
+  const diff = today - birthDate;
+  return Math.floor(diff / (1000 * 60 * 60 * 24));
+}
+
+console.log(ageInDays(2015, 11, 1))
